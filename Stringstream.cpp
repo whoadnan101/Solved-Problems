d6794b43e17228b126ac596d00fe8c77f59b149c
whoadnan101 <adnankaiser12@gmail.com>
2022-07-08 22:53:56 +0530

    Solved Problems

---
+++ Stringstream.cpp
@@ -0,0 +1,17 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    for(int i=0;i<str.size();i++){
+        if(str[i]!=','){
+            cout<<str[i];
+        }
+        else{
+            cout<<endl;
+        }
+    }
+}

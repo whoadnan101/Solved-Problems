33cfa75d6f46e21470139e3c1912b2f7c0044090
whoadnan101 <adnankaiser12@gmail.com>
2022-09-03 22:26:14 +0530

    All divisors of a given number.

---
+++ Divisors.cpp
@@ -0,0 +1,12 @@
+//All Divisors of a given number.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+   for(int i=1;i<n;i++){
+      if(n%i==0){
+      cout<<i<<endl;
+      }
+   }
+}

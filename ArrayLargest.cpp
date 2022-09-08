c64a205a257d265aa4004edbbfcaf676c649b009
whoadnan101 <adnankaiser12@gmail.com>
2022-09-08 10:11:15 +0530

    Geeksforgeeks

---
+++ ArrayLargest.cpp
@@ -0,0 +1,12 @@
+//Largest in an array
+int largest(vector<int> &arr, int n)
+    {
+        int max=arr[0];
+        for(int i=0;i<n;i++){
+        if(arr[i]>max){
+        max=arr[i];
+        }
+    }
+        return max;
+    }
+};

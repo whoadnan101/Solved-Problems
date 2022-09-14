bd62cac5498eee248f3100a2f890c561f997a83c
whoadnan101 <adnankaiser12@gmail.com>
2022-09-14 23:33:43 +0530

    Practice

---
+++ PowerofTwo.cpp
@@ -0,0 +1,9 @@
+//Power of two
+bool isPowerOfTwo(int n) {
+        for(int i=0;i<=30;i++){
+            int res=pow(2,i);
+            if(res==n)
+            return true;
+        }
+        return false;
+    }

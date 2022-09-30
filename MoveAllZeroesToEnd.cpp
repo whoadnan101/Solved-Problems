7c1c5bd6ea1ad205ba7cb5c265e51cea87b89444
whoadnan101 <adnankaiser12@gmail.com>
2022-09-30 21:19:56 +0530

    GeeksforGeeks

---
+++ MoveAllZeroesToEnd.cpp
@@ -0,0 +1,24 @@
+/*Given an array arr[] of N positive integers. Push all the zeros of the given array to the right end of the array while maitaining the order of non-zero elements.
+
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 5, 0, 0, 4}
+Output: 3 5 4 0 0
+Explanation: The non-zero elements
+preserve their order while the 0
+elements are moved to the right.*/
+
+//CODE
+
+void pushZerosToEnd(int arr[], int n) {
+	    int j=0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]!=0){
+	         swap(arr[i],arr[j]);
+	         j++;
+	        }
+	    }
+	}

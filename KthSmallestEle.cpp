ee9c7e9e0a636d14e3809c38268c93301141a44f
whoadnan101 <adnankaiser12@gmail.com>
2022-10-02 22:39:31 +0530

    GeeksforGeeks

---
+++ KthSmallestEle.cpp
@@ -0,0 +1,24 @@
+/*Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7 
+
+Kth smallest element CODE */
+
+// arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[],int l, int r, int k) {
+        int n=r+1;
+        sort(arr,arr+n);
+        return arr[k-1];
+    }

57b31360f8869acfe7811fd172cc331af86b0990
whoadnan101 <adnankaiser12@gmail.com>
2023-10-03 15:24:38 +0530

    Solved Problem

---
+++ sum of series.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sum,n;
+    cout<<"Enter the value";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        sum=+i;
+    }
+    cout<<sum;
+}

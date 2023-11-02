e69bae590d85a8af5551e6d94b72667681099d60
whoadnan101 <adnankaiser12@gmail.com>
2023-11-02 14:47:04 +0530

    Leetcode

---
+++ RotateAndSorted.cpp
@@ -0,0 +1,12 @@
+//CHECK IF ARRAY IS ROTATED AND SORTED
+
+int count=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0])
+            count++;
+        return count<=1;
